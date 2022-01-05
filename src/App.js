d0abb2a8375cd8c88cb5fd4d02d8fa4d812b2061
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2022-01-05 12:57:41 +0500

    First Comment

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "rgb(53, 59, 55)", height: "100vh", display: "flex", justifyContent: "center" }}>
+      <div style={{height:"15vh", display:"flex", alignItems:"center", flexDirection:"column"}}>
+        <h1 style={{ color: "white" }}>Welcome To My App</h1>
+        <h3 style={{ color: "white" }}>What is your main focus today</h3>
+        <input style={{color:"white",height:"50px", paddingBottom:"10px", textAlign:"center", fontSize:"30px", border:"0px", border:"0px", outline:"none", backgroundColor:"rgb(53, 59, 55)", borderBottom:"2px Solid Black"}} placeholder='Type Something'></input>
+      </div>
     </div>
   );
 }
